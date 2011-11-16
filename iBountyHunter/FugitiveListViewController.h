03cdc348930d397822e87313065bf4293e2dd7b4
David Mackenzie <dmackenz1981@gmail.com>
2011-11-16 18:49:13 +1100

    Created iBountyHunter Project with two views ready for the Core Data tute.

---
+++ iBountyHunter/FugitiveListViewController.h
@@ -0,0 +1,16 @@
+//
+//  FugitiveListViewController.h
+//  iBountyHunter
+//
+//  Created by David Mackenzie on 16/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface FugitiveListViewController : UITableViewController {
+    
+}
+
+@end
