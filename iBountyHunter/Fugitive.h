6d26485d3fe6de12ffc2124094318d71fb7f8807
dmackhack <dmackenz1981@gmail.com>
2011-11-19 17:55:47 +1100

    Added Fugitive.h/.m

---
+++ iBountyHunter/Fugitive.h
@@ -0,0 +1,21 @@
+//
+//  Fugitive.h
+//  iBountyHunter
+//
+//  Created by David Mackenzie on 19/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Fugitive : NSManagedObject {
+@private
+}
+@property (nonatomic, retain) NSString * name;
+@property (nonatomic, retain) NSDecimalNumber * bounty;
+@property (nonatomic, retain) NSNumber * fugitiveID;
+@property (nonatomic, retain) NSString * desc;
+
+@end
